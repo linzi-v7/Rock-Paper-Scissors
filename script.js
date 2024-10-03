3e93736d0154bc700a72af2abe388cacc5760249
linzi-v7 <farisos7777@gmail.com>
2024-10-03 16:30:08 +0300

    Add playRound() function to play a single round of rock,paper,scissors

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ let computerScore = 0;
 function getComputerChoice()
 {
     //Research Math.Random() and how it works with Math.floor() to get random number from 0 to n-1, n being 3 here
-    let choice = Math.floor(Math.random() * 3);  
+    let choice = Math.floor(Math.random() * 3);
 
-    switch (choice) {
+    switch (choice)
+    {
         case 0:
             return "Rock";
             break;
@@ -28,19 +29,19 @@ function getHumanChoice()
     let choice = prompt("Choose between: Rock, Paper and Scissors. Type (R,P,S) for faster choice if you like.")
 
     choice = choice.toLowerCase();
-    let rock        =  (choice == "r" || choice == "rock");
-    let paper       =  (choice == "p" || choice == "paper");
-    let scissors    =  (choice == "s" || choice == "scissors");
+    let rock = (choice == "r" || choice == "rock");
+    let paper = (choice == "p" || choice == "paper");
+    let scissors = (choice == "s" || choice == "scissors");
 
-    if(rock)
+    if (rock)
     {
         return "Rock";
     }
-    else if(paper) 
+    else if (paper)
     {
         return "Paper";
     }
-    else if(scissors)
+    else if (scissors)
     {
         return "Scissors";
     }
@@ -48,5 +49,65 @@ function getHumanChoice()
     {
         console.log("INVALID CHOICE! REFRESH")
     }
-    
+
+}
+
+function playRound(humanChoice, computerChoice)
+{
+    let humanWin = false;
+    let computerWin = false;
+
+    if (humanChoice == "Rock")
+    {
+        switch (computerChoice)
+        {
+            case "Paper":
+                computerWin = true;
+                break;
+            case "Scissors":
+                humanWin = true;
+                break;
+        }
+    }
+
+    if (humanChoice == "Paper")
+    {
+        switch (computerChoice)
+        {
+            case "Rock":
+                humanWin = true;
+                break;
+            case "Scissors":
+                computerWin = true;
+                break;
+        }
+    }
+
+    if (humanChoice == "Scissors")
+    {
+        switch (computerChoice)
+        {
+            case "Paper":
+                humanWin = true;
+                break;
+            case "Rock":
+                computerWin = true;
+                break;
+        }
+    }
+
+
+    if (humanWin)
+    {
+        humanScore++;
+        console.log("You win! " + humanChoice + " beats" + computerChoice);
+    }
+    else if (computerWin)
+    {
+        computerScore++;
+        console.log("You lose! " + computerChoice + " beats" + humanChoice);
+
+    }
+
+    console.log("Current human score: " + humanScore + "\nCurrent Computer Score: " + computerScore);
 }
